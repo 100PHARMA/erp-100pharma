4975ae4f6ceb559d5680df299518192f831930d0
100PHARMA <marioduarteempresas@gmail.com>
2026-01-04 22:27:17 +0000

    Update layout.tsx

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -25,9 +25,9 @@ export default async function AdminLayout({
   if (role !== 'ADMIN') redirect('/portal');
 
   return (
-    <>
-      <NavbarGate />
-      <main className="min-h-[calc(100vh-4rem)]">{children}</main>
-    </>
-  );
+  <div style={{ padding: 24 }}>
+    <div>ADMIN LAYOUT OK</div>
+    {children}
+  </div>
+);
 }
