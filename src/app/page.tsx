ed46faf85280610dc9601ecdaeb0abf90fd0a668
100PHARMA <marioduarteempresas@gmail.com>
2026-01-05 15:12:33 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div style={{ padding: 24 }}>OK</div>;
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>ERP 100PHARMA</h1>
+      <a href="/login">Ir para login</a>
+    </div>
+  );
 }
